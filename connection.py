e387f77179d6947d035c2d9c80eed7f65e98965e
plaivny <pwisoziaodioan@gmail.com>
2025-11-12 19:30:30 -0300

    update dockerfile

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ dotenv.load_dotenv()
 
 connection = connect(
     host=os.getenv("DATABASE_HOST"),
-    port=int(os.getenv("DATABESE_PORT")),
+    port=int(os.getenv("DATABASE_PORT")),
     user=os.getenv("DATABASE_USER"),
     password=os.getenv("DATABASE_PASS"),
     database=os.getenv("DATABASE_DB"),

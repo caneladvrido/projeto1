28f7944293efb330acb8c7aed539642edabb40eb
plaivny <pwisoziaodioan@gmail.com>
2025-11-12 21:05:49 -0300

    update dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from rotas.personagens import rota_personagens
-from rotas.raca import rota_raça
+from rotas.raca import rota_raca
 
 app= FastAPI()
 
 app.include_router(rota_personagens)
-app.include_router(rota_raça)
+app.include_router(rota_raca)
